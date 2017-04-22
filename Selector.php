313b7f7b102577f85f4bf1e4e560baed864c8513
Xavier Chopin <xchopin54@gmail.com>
2017-04-22 18:32:51 +0200

    Added Selector class

--- Selector.php
+++ Selector.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Xavier
- * Date: 22/04/2017
- * Time: 18:03
- */
 
 namespace Chopin\EasyXML;
 
-
+/**
+ * Selector
+ *
+ * @author  Xavier Chopin <bonjour@xavierchop.in>
+ * @package Chopin\EasyXML
+ * @license MIT
+ */
 class Selector
 {
+    protected $file;
+
+    protected $data;
+
+    /**
+     * Create a new Selector
+     * @param String $file
+     */
+    public function __construct($file)
+    {
+        $this->file = $file;
+        $this->data = simplexml_load_file($file);
+
+        if (!$this->data)
+            print_r('<h3> <strong><u>EasyXML Error:</u></strong> XML file was not found! </h3>');
+    }
+
+    /**
+     * Count the node of the file loaded
+     * @param String $element
+     */
+    public function countElement($element) {
+        $dom = new DOMDocument;
+        $dom->loadXml($this->data);
+        return $dom->getElementsByTagName($element)->length;
+    }
 
 }

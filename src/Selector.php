0088dcac02547989a2c7e04810eb12c32dcb9eda
Xavier Chopin <xchopin54@gmail.com>
2017-04-24 10:35:11 +0200

    Added first() and updated get()

--- src/Selector.php
+++ src/Selector.php
@@ -48,18 +48,55 @@ class Selector
     }
 
     /**
-     * Count the node of the file loaded
+     * Count the nodes of the file loaded or the node given
      *
      * @param String $element
      */
-    public function countElement($element)
+    public function countElement($element = null)
+    {
+        if ($element != null) {
+            if (is_string($element))
+                return count($this->data->$element);
+            else
+                return self::prettyError('Element given is not a String!');
+        }else {
+            $count = 0;
+            foreach ($this->data as $element) {
+                $count++;
+                $count+= $element->count();
+            }
+            return $count;
+        }
+    }
+
+    /**
+     * Return the first element given (object)
+     *
+     * @param $element
+     */
+    public function first($element)
     {
         if (is_string($element))
-            return count($this->data->$element);
+            return $this->data->$element;
         else
             return self::prettyError('Element given is not a String!');
     }
 
+    /**
+     * Return the elements given (object)
+     *
+     * @param $element
+     */
+    public function get($element)
+    {
+        if (!is_string($element))
+            return self::prettyError('Element given is not a String!');
+       $res = array();
 
+       foreach ($this->data->$element as $child)
+           array_push($res, $child);
 
+       return $res;
+    }
+    
 }
